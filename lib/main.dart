d314d31a9cb94f5d5d9379699d34ce6db9fb76f5
hixiaoan <xiaoanwuios@163.com>
2022-07-12 18:53:55 +0800

    重构目录

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,20 @@
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:orientation/orientation.dart';
-import 'package:uu_app/modules/home_module/child_pages/video_detail_page.dart';
-import 'package:uu_app/util/cache_util.dart';
-import 'package:uu_app/util/routers_init.dart';
-import 'package:uu_app/uu_provider/providers.dart';
-import 'package:uu_app/uu_provider/theme_provider.dart';
-
-import 'mian_page/main_page.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-
-
-final cache = UUCache.preInit();
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
+import 'package:uu_app/theme.dart';
+import 'common/app_router/index.dart';
+import 'common/i18n/index.dart';
+import 'common/store/index.dart';
+import 'common/widgets/index.dart';
+import 'global.dart';
 
 void main() {
-
-  ///获取设备信息,全屏
-  WidgetsFlutterBinding.ensureInitialized();
-  OrientationPlugin.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  OrientationPlugin.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-  runApp(
-      MultiProvider(
-          providers: appRroviders,
-          child: const MyApp()
-          )
-
-  );
+  Global.init().then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -39,38 +28,113 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<UUCache>(
-        future: cache,
-        builder: (context, snapshot) {
+    return ScreenUtilInit(
+       designSize: const Size(428, 926),
+      builder:(context, child) =>  RefreshConfiguration(
+        headerBuilder:()=> const ClassicHeader(
+          refreshingIcon: CupertinoActivityIndicator(),
+        ),
+        footerBuilder: ()=>const ClassicFooter(
+          loadingIcon: CupertinoActivityIndicator(),
+        ),
+        hideFooterWhenNotFull: true,
+        child:GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter UU',
+          theme: AppTheme.light(),
+          darkTheme: AppTheme.dark(),
+          themeMode: AppTheme.mode,
+          popGesture: true,
+          transitionDuration: const Duration(milliseconds: 450),
+          customTransition: RouteTransition(),
+          initialRoute: RouterName.Launch,
+          getPages: Routers.getPages,
+          navigatorObservers: [Routers.observer],
+          builder: EasyLoading.init(builder: (context, child) {
+            EasyLoading.instance
+              ..displayDuration = const Duration(milliseconds: 2000)
+              ..indicatorType = EasyLoadingIndicatorType.ring
+              ..loadingStyle = EasyLoadingStyle.custom
+              ..radius = 20.w
+              ..boxShadow = [
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.shadow.withOpacity(0.15),
+                  offset: const Offset(0, 0),
+                  blurRadius: 20.w,
+                ),
+              ]
+              ..progressColor = Colors.transparent
+              ..contentPadding = EdgeInsets.all(20.w)
+              ..backgroundColor =
+                  Theme.of(context).brightness == Brightness.light
+                      ? Theme.of(context).colorScheme.surface
+                      : Theme.of(context).colorScheme.tertiary
+              ..indicatorColor = Colors.transparent
+              ..textColor = Theme.of(context).colorScheme.onSurface
+              ..textStyle = TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 17.w,
+                color: Theme.of(context).colorScheme.onSurface,
+              )
+              ..maskType = EasyLoadingMaskType.clear
+              ..maskColor = const Color(0xFF09101D).withOpacity(0.7)
+              ..userInteractions = true
+              ..successWidget = const CustomToastSuccess()
+              ..errorWidget = const CustomToastFail()
+              ..indicatorWidget = CustomLoadingIndicator(size: 60.w)
+              ..dismissOnTap = false;
+            return ScrollConfiguration(
+              behavior: NoShadowScrollBehavior(),
+              child: child ?? const Material(),
+            );
+          }),
+          // localizationsDelegates: const [
+          //   GlobalMaterialLocalizations.delegate,
+          //   GlobalWidgetsLocalizations.delegate,
+          //   GlobalCupertinoLocalizations.delegate,
+          // ],
+          supportedLocales: Translation.supported,
+          fallbackLocale: Translation.fallback,
+          locale: ConfigStore.to.locale,
+          translations: Translation(),
+        ),
+      )
 
-          StatefulWidget widget;
-          if (snapshot.connectionState == ConnectionState.done){
-            widget =  const MainWidget();
-          }else{
-            widget = const Scaffold(
-              body:Center(
-                child: Text("还在加载异步任务"),
-              ),
-            );
-          }
-          return Consumer<ThemeProvider>(builder:(ctx,provider,child){
-            return MaterialApp(
-              routes: uuAppRoutersInit(),
-              onGenerateRoute: (settings){
-                if (settings.name == VideoDetailPage.routerName){
-                  return MaterialPageRoute(builder: (ctx){
-                    return VideoDetailPage(videoId: settings.arguments as String);
-                  });
-                }
-                return null;
-              },
-              darkTheme: provider.getThemeData(isDarkMode: true),
-              themeMode: provider.getThemeMode(),
-              theme: provider.getThemeData(),
-              home: widget,
-            );
-          });
-        });
+      )
+    ;
+  }
+}
+
+class NoShadowScrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+    BuildContext context,
+    Widget child,
+    AxisDirection axisDirection,
+  ) {
+    switch (getPlatform(context)) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+        return child;
+      case TargetPlatform.android:
+        return GlowingOverscrollIndicator(
+          showLeading: false,
+          showTrailing: false,
+          axisDirection: axisDirection,
+          color: Theme.of(context).colorScheme.primary,
+          child: child,
+        );
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return GlowingOverscrollIndicator(
+          showLeading: false,
+          showTrailing: false,
+          axisDirection: axisDirection,
+          color: Theme.of(context).colorScheme.primary,
+          child: child,
+        );
+    }
   }
 }
 
